8cdbb0fb220cc2d783e3d965234b73acac744c0c
Chandrasekar Sureshkumar <chandrusuresh@gmail.com>
2018-09-26 10:46:53 -0400

    Solution to Computing marginals
    Putting all the code together to compute marginals.

--- wk2/ComputeExactMarginalsBP.m
+++ wk2/ComputeExactMarginalsBP.m
@@ -16,12 +16,35 @@ function M = ComputeExactMarginalsBP(F, E, isMax)
 
 % initialization
 % you should set it to the correct value in your code
-M = [];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %
 % Implement Exact and MAP Inference.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+P = CreateCliqueTree(F, E);
+Pc = CliqueTreeCalibrate(P, isMax);
 
+V = unique([F(:).var]);
+templateFactor = struct();
+templateFactor.var = [];
+templateFactor.card = [];
+templateFactor.val = [];
+for i = 1:length(V)
+    M(i) = templateFactor;
+end
+
+neighbors = sum(Pc.edges,1);
+[srt_neighbors,ind] = sort(neighbors);
+
+for i = 1:length(ind)
+    node = ind(i);
+    scope = Pc.cliqueList(node).var;
+    for j = 1:length(scope)
+        if isempty(M(scope(j)).var)
+            varsToSumOut = setdiff(scope,scope(j));
+            M(scope(j)) = FactorMarginalization(Pc.cliqueList(node),varsToSumOut);
+            M(scope(j)).val = M(scope(j)).val/sum(M(scope(j)).val);
+        end
+    end
 end
